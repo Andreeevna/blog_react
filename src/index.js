5be71ead7a0dc7b35e9427e252158f919941487c
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-16 16:36:59 +0300

    feat: implemented getting articles using redux

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { ThemeProvider } from '@mui/material'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import './index.scss'
+import store from './redux/store'
 import { theme } from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-	<React.StrictMode>
+	<>
 		<CssBaseline />
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 			</BrowserRouter>
 		</ThemeProvider>
-	</React.StrictMode>
+	</>
 )
